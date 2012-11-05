658018c988ce4bd8b29d8073244b6fcfafa0d2eb
rmajasol <tishobueno@gmail.com>
2012-11-05 19:52:28 +0100

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -25,6 +25,7 @@ group :test do
 	gem 'rspec-rails', '2.9.0'
 	gem 'guard-spork', '0.3.2'
   gem 'spork', '0.9.0'
+  gem 'factory_girl_rails'
 	# System-dependent gems
 	gem 'rb-fsevent', :require => false 
 	gem 'growl', '1.0.3'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -72,6 +72,15 @@ describe User do
 		it { should_not be_valid }
 	end
 
+	describe "email address with mixed case" do
+		let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			@user.reload.email.should == mixed_case_email.downcase
+		end 
+	end
+
 	describe "when password is not present" do
 		before { @user.password = @user.password_confirmation = " " } 
 		it { should_not be_valid }
@@ -95,7 +104,7 @@ describe User do
 	describe "return value of authenticate method" do 
 		before { @user.save }
 		let(:found_user) { User.find_by_email(@user.email) }
-		
+
 		describe "with valid password" do
 			it { should == found_user.authenticate(@user.password) }
 		end
