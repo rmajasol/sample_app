36e620fd9d49bee08faf80541829e1c76f3a0e69
rmajasol <tishobueno@gmail.com>
2012-11-13 16:28:20 +0100

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ActiveRecord::Base
 	before_save { self.email.downcase! }
 	before_save :create_remember_token
 
+	# validations
 	validates :name, presence: true, length: { maximum: 50 } 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, 
@@ -25,7 +26,7 @@ class User < ActiveRecord::Base
 	validates :password_confirmation, presence: true
 
 	private
-	def create_remember_token
-		self.remember_token = SecureRandom.urlsafe_base64
-	end
+		def create_remember_token
+			self.remember_token = SecureRandom.urlsafe_base64
+		end
 end
