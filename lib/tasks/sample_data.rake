b876c6de19fbdfd4a7f861060cd3e3c9ebaa3592
rmajasol <tishobueno@gmail.com>
2012-11-14 20:53:23 +0100

    Add user microposts

--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -1,11 +1,17 @@
 namespace :db do
 	desc "Fill database with sample data" 
 	task populate: :environment do
+
+		###
+		# an admin user
 		admin = User.create!(	name: "Example User",
 													email: "example@railstutorial.org",
 													password: "foobar",
 													password_confirmation: "foobar")
 		admin.toggle!(:admin)		
+		
+		###
+		# 99 more users (without being admin)
 		99.times do |n|
 			name  = Faker::Name.name
 			email = "example-#{n+1}@railstutorial.org" 
@@ -14,6 +20,14 @@ namespace :db do
 				email: email,
 				password: password,
 				password_confirmation: password)
+		end
+
+		###
+		# 50 microposts on each 6 first users
+		users = User.all(limit: 6)
+		50.times do
+			content = Faker::Lorem.sentence(5) # returns "lorem ipsum"
+			users.each { |user| user.microposts.create!(content: content) }
 		end
 	end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,44 +17,59 @@ describe "Static_pages" do
 		it_should_behave_like "all static pages"
 
 		it { should_not have_selector 'title', text: '| Home' }
-	end
 
-	describe "Help page" do
-		before { visit help_path }
-		let(:heading) { 'Help' }
-		let(:page_title) { 'Help' }
+		describe "for signed-in users" do
+			let(:user) { FactoryGirl.create(:user) } 
+			before do
+				FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum") 
+				FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet") 
+				sign_in user
+				visit root_path
+			end
+			it "should render the user's feed" do 
+				user.feed.each do |item|
+					page.should have_selector("li##{item.id}", text: item.content) 
+				end
+			end
+		end
 
-		it_should_behave_like "all static pages"
-	end
+		describe "Help page" do
+			before { visit help_path }
+			let(:heading) { 'Help' }
+			let(:page_title) { 'Help' }
 
-	describe "About page" do
-		before { visit about_path }
-		let(:heading) { 'About' }
-		let(:page_title) { 'About Us' }
+			it_should_behave_like "all static pages"
+		end
 
-		it_should_behave_like "all static pages"
-	end
+		describe "About page" do
+			before { visit about_path }
+			let(:heading) { 'About' }
+			let(:page_title) { 'About Us' }
 
-	describe "Contact page" do 
-		before { visit contact_path }
-		let(:heading) { 'Contact' }
-		let(:page_title) { 'Contact' }
+			it_should_behave_like "all static pages"
+		end
 
-		it_should_behave_like "all static pages"
-	end
+		describe "Contact page" do 
+			before { visit contact_path }
+			let(:heading) { 'Contact' }
+			let(:page_title) { 'Contact' }
 
-	it "should have the right links on the layout" do
-		visit root_path
-		click_link "About"
-		page.should have_selector 'title', text: full_title('About Us')
-		click_link "Help"
-		page.should have_selector 'title', text: full_title('Help')
-		click_link "Contact"
-		page.should have_selector 'title', text: full_title('Contact')
-		click_link "Home"
-		click_link "Sign up now!"
-		page.should have_selector 'title', text: full_title('Sign up')
-		click_link "sample app"
-		page.should have_selector 'title', text: full_title('')
+			it_should_behave_like "all static pages"
+		end
+
+		it "should have the right links on the layout" do
+			visit root_path
+			click_link "About"
+			page.should have_selector 'title', text: full_title('About Us')
+			click_link "Help"
+			page.should have_selector 'title', text: full_title('Help')
+			click_link "Contact"
+			page.should have_selector 'title', text: full_title('Contact')
+			click_link "Home"
+			click_link "Sign up now!"
+			page.should have_selector 'title', text: full_title('Sign up')
+			click_link "sample app"
+			page.should have_selector 'title', text: full_title('')
+		end
 	end
 end
